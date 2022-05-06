5ea45d616233b71525cda3e9a9b6a16677716c1a
Majba <monaiburkhan@gmail.com>
2022-05-07 05:37:20 +0600

    added manage

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require("express");
+ const express = require("express");
 const cors = require('cors');
 const jwt =require('jsonwebtoken')
 const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
@@ -10,24 +10,24 @@ const app =express()
 app.use(cors())
 app.use(express.json())
 
-function verifyJWT(req, res, next) {
-    const authHeader = req.headers.authorization;
-    if (!authHeader) {
-        return res.status(401).send({ message: 'unauthorized access' });
-    }
-    const token = authHeader.split(' ')[1];
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-        if (err) {
-            return res.status(403).send({ message: 'Forbidden access' });
-        }
-        console.log('decoded', decoded);
-        req.decoded = decoded;
-        next();
-    })
-}
+// function verifyJWT(req, res, next) {
+//     const authHeader = req.headers.authorization;
+//     if (!authHeader) {
+//         return res.status(401).send({ message: 'unauthorized access' });
+//     }
+//     const token = authHeader.split(' ')[1];
+//     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+//         if (err) {
+//             return res.status(403).send({ message: 'Forbidden access' });
+//         }
+//         console.log('decoded', decoded);
+//         req.decoded = decoded;
+//         next();
+//     })
+// }
 
 
-
+ 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oiyjm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run(){
@@ -35,135 +35,67 @@ async function run(){
         await client.connect()
         const productCollection = client.db("dreamsvehicle").collection("product");
         const delearCollection = client.db("dreamsvehicle").collection("delear");
-        const orderCollection = client.db("dreamsvehicle").collection("order");
 
 
-        // app.get('/product',async(req,res)=>{
-        //     const query={}
-        //     const cursor=productsCollection.find(query)
-        //     const products=await cursor.toArray()
-        //     res.send(products)
-        // })
-        // app.get('/product/:id',async(req,res)=>{
-        //     const id =req.params.id
-        //     const query={_id: ObjectId(id)}
-        //     const product=await productsCollection.findOne(query)
-        //     res.send(product)
-
-
-        // })
-        
-        // app.post('/product', async(req, res) =>{
-        //     const newProduct = req.body;
-        //     const result = await productsCollection.insertOne(newProduct);
-        //     res.send(result);
-        // });
-
-        // app.post('/login', async (req, res) => {
-        //     const user = req.body;
-        //     const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-        //         expiresIn: '1d'
-        //     });
-        //     res.send({ accessToken });
-        // })
-
-        // // DELETE
-        // app.delete('/product/:id', async (req, res) => {
-        //     const id = req.params.id;
-        //     const query = { _id: ObjectId(id) };
-        //     const result = await productsCollection.deleteOne(query);
-        //     res.send(result);
-        // });
-        
-        // app.get('/delear',async(req,res)=>{
-        //     const query={}
-        //     const cursor=delearCollection.find(query)
-        //     const delear=await cursor.toArray()
-        //     res.send(delear)
-        // })
-
-        // app.get('/order', async (req, res) => {
-        //     const decodedEmail = req.decoded.email;
-        //     const email = req.query.email;
-        //     if (email === decodedEmail) {
-        //         const query = { email: email };
-        //         const cursor = orderCollection.find(query);
-        //         const orders = await cursor.toArray();
-        //         res.send(orders);
-        //     }
-        //     else{
-        //         res.status(403).send({message: 'forbidden access'})
-        //     }
-        // })
-
-        // app.post('/order', async (req, res) => {
-        //     const order = req.body;
-        //     const result = await orderCollection.insertOne(order);
-        //     res.send(result);
-        // })
-
-
-
-        // AUTH
-        app.post('/login', async (req, res) => {
-            const user = req.body;
-            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-                expiresIn: '1d'
-            });
-            res.send({ accessToken });
-        })
-
-        // productS API
-        app.get('/product', async (req, res) => {
+        app.get('/items', async (req, res) => {
             const query = {};
             const cursor = productCollection.find(query);
-            const products = await cursor.toArray();
-            res.send(products);
-        });
 
-        app.get('/product/:id', async (req, res) => {
-            const id = req.params.id;
-            console.log(id);
-            const query = { _id: ObjectId(id) };
-            const product = await productCollection.findOne(query);
-            res.send(product);
-        });
+            const page = parseInt(req.query.page);
+            const size = parseInt(req.query.size);
 
-        // POST
-        app.post('/product', async (req, res) => {
-            const newProduct = req.body;
-            const result = await productCollection.insertOne(newProduct);
-            res.send(result);
-        });
+            let items;
 
-        // DELETE
-        app.delete('/product/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: ObjectId(id) };
-            const result = await productCollection.deleteOne(query);
-            res.send(result);
-        });
+            if (page || size) 
+            {
 
-        // Order Collection API
-
-        app.get('/order', verifyJWT, async (req, res) => {
-            const decodedEmail = req.decoded.email;
-            const email = req.query.email;
-            if (email === decodedEmail) {
-                const query = { email: email };
-                const cursor = orderCollection.find(query);
-                const orders = await cursor.toArray();
-                res.send(orders);
+                items = await cursor.skip(page*size).limit(size).toArray();
             }
-            else{
-                res.status(403).send({message: 'forbidden access'})
+
+            else {
+                items = await cursor.toArray();
             }
+
+            res.send(items)
         })
 
-        app.post('/order', async (req, res) => {
-            const order = req.body;
-            const result = await orderCollection.insertOne(order);
+
+        app.get('/itemsCount', async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const count = await productCollection.estimatedDocumentCount();
+            res.send({ count })
+        })
+
+        app.get("/item/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await productCollection.findOne(query);
             res.send(result);
+        })
+
+
+        app.put("/delivered/:id", async (req, res) => {
+            const id = req.params.id;
+            const updatedItem = req.body;
+
+            console.log(updatedItem);
+
+            const filter = { _id: ObjectId(id) };
+
+            const options = { upsert: true };
+
+            const updatedDoc = {
+                $set: {
+                    quantity: updatedItem.quantity,
+                    sale: updatedItem.sale
+                }
+            }
+
+            const result = await productCollection.updateOne(filter, updatedDoc, options);
+
+            res.send(result);
+
         })
         app.get('/delear',async(req,res)=>{
             const query={}
@@ -171,8 +103,57 @@ async function run(){
             const delear=await cursor.toArray()
             res.send(delear)
         })
+
+        app.put("/restock/:id", async (req, res) => {
+            const id = req.params.id;
+            const updatedItem = req.body;
+
+
+            const filter = { _id: ObjectId(id) };
+
+            const options = { upsert: true };
+
+            const updatedDoc = {
+                $set: {
+                    Quantity: updatedItem.Quantity
+                }
+            }
+
+            const result = await productCollection.updateOne(filter, updatedDoc, options);
+
+            res.send(result);
+
+        })
+
+        // app.post("/addnewitem" , async(req,res)=>
+        // {
+        //     const newItem = req.body;
+        //     const result = await productCollection.insertOne(newItem)
+
+        //     res.send(result);
+        // })
+
+
+        // app.post("/deleteitem" , async(req , res)=>
+        // {
+        //     const id = req.body;
+
+        //     const result = await productCollection.deleteOne({ "_id" : ObjectId(id)})
+
+        //     res.send(result)
+        // })
+        app.delete('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await productCollection.deleteOne(query);
+            res.send(result);
+        });
+
     }
-    finally{}
+
+    finally {
+        /* await client.close(); */
+    }
 }
 run().catch(console.dir)
 
